6569ea7b9c450eac3d2ce1d859253af945c0f78d
Frisk69 <vasahun@gmail.com>
2020-04-27 16:00:43 +0200

    Update the_end.py

--- the_end.py
+++ the_end.py
@@ -1,8 +1,17 @@
 
-
+import random
+from random import randint
 done = False
 import time
 
+aids = {
+    "I'll fuckin' do it!": 2,
+    "I'll do it!":2,
+    "I won't puss out this time!": 2,
+    "I can't take it no more.": 2
+    }
+asshole = random.randint(0,3)
+Rodeo = aids.value
 while not done:
     f= open("nuuse.txt", "r")
     if f.mode =="r":
@@ -11,6 +20,7 @@ while not done:
 
         print(contents)
         print("")
+        print(Rodeo)
         print("imma hang myself")
         time.sleep(0.2)
         
