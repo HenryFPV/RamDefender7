b9ae30ece4dd176cd62f7a50c120bbcb212303d2
HenryFPV <BABMAN2@users.noreply.github.com>
2020-04-27 14:45:43 +0300

    added files for drn

---
+++ DUMP/secproto.bat
@@ -0,0 +1,11 @@
+
+@echo off
+color 0a
+title set mainframe check v8.2
+pause
+color 0a
+mode 1000
+
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
